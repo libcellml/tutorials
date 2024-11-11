e56d0d6e7453ad80e1d80cfdc2a257cb73284754
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-11-11 20:41:08 +1300

    Update generated code for tutorial 3.

--- tutorial3/expected_results/PredatorPrey.c
+++ tutorial3/expected_results/PredatorPrey.c
@@ -1,12 +1,12 @@
-/* The content of this file was generated using the C profile of libCellML 0.4.0. */
+/* The content of this file was generated using the C profile of libCellML 0.6.3. */
 
 #include "PredatorPrey.h"
 
 #include <math.h>
 #include <stdlib.h>
 
-const char VERSION[] = "0.4.0";
-const char LIBCELLML_VERSION[] = "0.4.0";
+const char VERSION[] = "0.5.0";
+const char LIBCELLML_VERSION[] = "0.6.3";
 
 const size_t STATE_COUNT = 2;
 const size_t VARIABLE_COUNT = 4;
@@ -14,13 +14,13 @@ const size_t VARIABLE_COUNT = 4;
 const VariableInfo VOI_INFO = {"time", "month", "predator_prey_component", VARIABLE_OF_INTEGRATION};
 
 const VariableInfo STATE_INFO[] = {
-    {"y_s", "number_of_sharks", "predator_prey_component", STATE},
-    {"y_f", "thousands_of_fish", "predator_prey_component", STATE}
+    {"y_f", "thousands_of_fish", "predator_prey_component", STATE},
+    {"y_s", "number_of_sharks", "predator_prey_component", STATE}
 };
 
 const VariableInfo VARIABLE_INFO[] = {
-    {"c", "per_month", "predator_prey_component", COMPUTED_CONSTANT},
     {"a", "per_month", "predator_prey_component", CONSTANT},
+    {"c", "per_month", "predator_prey_component", COMPUTED_CONSTANT},
     {"b", "per_shark_month", "predator_prey_component", CONSTANT},
     {"d", "per_1000fish_month", "predator_prey_component", CONSTANT}
 };
@@ -54,22 +54,22 @@ void deleteArray(double *array)
 
 void initialiseVariables(double *states, double *rates, double *variables)
 {
-    variables[1] = -0.8;
+    variables[0] = -0.8;
     variables[2] = 0.3;
     variables[3] = -0.6;
-    states[0] = 1.0;
-    states[1] = 2.0;
+    states[0] = 2.0;
+    states[1] = 1.0;
 }
 
 void computeComputedConstants(double *variables)
 {
-    variables[0] = variables[1]+2.0;
+    variables[1] = variables[0]+2.0;
 }
 
 void computeRates(double voi, double *states, double *rates, double *variables)
 {
-    rates[0] = variables[1]*states[0]+variables[2]*states[0]*states[1];
-    rates[1] = variables[0]*states[1]+variables[3]*states[0]*states[1];
+    rates[1] = variables[0]*states[1]+variables[2]*states[1]*states[0];
+    rates[0] = variables[1]*states[0]+variables[3]*states[1]*states[0];
 }
 
 void computeVariables(double voi, double *states, double *rates, double *variables)
--- tutorial3/expected_results/PredatorPrey.py
+++ tutorial3/expected_results/PredatorPrey.py
@@ -1,11 +1,11 @@
-# The content of this file was generated using the Python profile of libCellML 0.4.0.
+# The content of this file was generated using the Python profile of libCellML 0.6.3.
 
 from enum import Enum
 from math import *
 
 
 __version__ = "0.4.0"
-LIBCELLML_VERSION = "0.4.0"
+LIBCELLML_VERSION = "0.6.3"
 
 STATE_COUNT = 2
 VARIABLE_COUNT = 4
@@ -22,13 +22,13 @@ class VariableType(Enum):
 VOI_INFO = {"name": "time", "units": "month", "component": "predator_prey_component", "type": VariableType.VARIABLE_OF_INTEGRATION}
 
 STATE_INFO = [
-    {"name": "y_s", "units": "number_of_sharks", "component": "predator_prey_component", "type": VariableType.STATE},
-    {"name": "y_f", "units": "thousands_of_fish", "component": "predator_prey_component", "type": VariableType.STATE}
+    {"name": "y_f", "units": "thousands_of_fish", "component": "predator_prey_component", "type": VariableType.STATE},
+    {"name": "y_s", "units": "number_of_sharks", "component": "predator_prey_component", "type": VariableType.STATE}
 ]
 
 VARIABLE_INFO = [
-    {"name": "c", "units": "per_month", "component": "predator_prey_component", "type": VariableType.COMPUTED_CONSTANT},
     {"name": "a", "units": "per_month", "component": "predator_prey_component", "type": VariableType.CONSTANT},
+    {"name": "c", "units": "per_month", "component": "predator_prey_component", "type": VariableType.COMPUTED_CONSTANT},
     {"name": "b", "units": "per_shark_month", "component": "predator_prey_component", "type": VariableType.CONSTANT},
     {"name": "d", "units": "per_1000fish_month", "component": "predator_prey_component", "type": VariableType.CONSTANT}
 ]
@@ -43,20 +43,20 @@ def create_variables_array():
 
 
 def initialise_variables(states, rates, variables):
-    variables[1] = -0.8
+    variables[0] = -0.8
     variables[2] = 0.3
     variables[3] = -0.6
-    states[0] = 1.0
-    states[1] = 2.0
+    states[0] = 2.0
+    states[1] = 1.0
 
 
 def compute_computed_constants(variables):
-    variables[0] = variables[1]+2.0
+    variables[1] = variables[0]+2.0
 
 
 def compute_rates(voi, states, rates, variables):
-    rates[0] = variables[1]*states[0]+variables[2]*states[0]*states[1]
-    rates[1] = variables[0]*states[1]+variables[3]*states[0]*states[1]
+    rates[1] = variables[0]*states[1]+variables[2]*states[1]*states[0]
+    rates[0] = variables[1]*states[0]+variables[3]*states[1]*states[0]
 
 
 def compute_variables(voi, states, rates, variables):
